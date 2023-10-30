32eddbda67abe40721a83ac4e23c43b904ba9b0a
Oussama kersit <kersiit@gmail.com>
2023-10-30 12:24:11 +0100

    Fixing styling of nav tag

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,62 +1,38 @@
 import { Link } from 'react-router-dom';
 import MyLogo from '../../assets/Maghrebin_logo.svg';
+import links from './links';
+import iconLinks from './iconLinks';
 
 function NavBar() {
   return (
     <div className="flex m-6">
       {/* Menu div */}
-      <div className="flex">
-        <nav>
-          <ul className="flex justify-start p-4">
-            <li>
-              <Link to="/home" className="text-neutral-900 hover:text-neutral-800 uppercase font-medium m-0">
-                Home
+      <nav className="flex justify-center">
+        <ul className="flex justify-between overflow-hidden w-72 p-4">
+          {links.map(link => (
+            <li className="w-fit" key={link.name}>
+              <Link to={link.path} className="text-neutral-900 hover:text-neutral-800 uppercase font-medium m-0">
+                {link.name}
               </Link>
             </li>
-            <li>
-              <Link to="/men" className="text-neutral-900 hover:text-neutral-800 uppercase font-medium m-0">
-                Men
-              </Link>
-            </li>
-            <li>
-              <Link to="/woman" className="text-neutral-900 hover:text-neutral-800 uppercase font-medium m-0">
-                Woman
-              </Link>
-            </li>
-            <li>
-              <Link to="/kids" className="text-neutral-900 hover:text-neutral-800 uppercase font-medium m-0">
-                Kids
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+          ))}
+        </ul>
+      </nav>
 
       {/* Logo div */}
       <div>
-        <Link to="/home">
+        <Link to="/ ">
           <img src={MyLogo} alt="MyLogo" className="h-12" />
         </Link>
       </div>
 
       {/* Icons div */}
       <div className="flex items-center space-x-3 justify-center">
-        {/* Adjusted the spacing here */}
-        <Link to="#" className="w-8">
-          <img src="/src/assets/searchIcon.svg" alt="searchIcon" className="h-6 m-0 p-0" />
-        </Link>
-        <Link to="#" className="w-8">
-          <img src="/src/assets/favIcon.svg" alt="favIcon" className="h-6" />
-        </Link>
-        <Link to="#" className="w-8">
-          <img src="/src/assets/cartIcon.svg" alt="searchIcon" className="h-6" />
-        </Link>
-        <div className="w-8">
-          <img src="/src/assets/navLine_separator.svg" alt="navLine_separator" className="h-6" />
-        </div>
-        <Link to="#" className="w-8">
-          <img src="/src/assets/userIcon.svg" alt="userIcon" className="h-6" />
-        </Link>
+        {iconLinks.map((iconLink, index) => (
+          <Link key={index} to={iconLink.path} className="w-8">
+            <img src={iconLink.icon} alt={iconLink.alt} className="h-6 m-0 p-0" />
+          </Link>
+        ))}
       </div>
     </div>
   );
---
+++ src/components/navbar/iconLinks.js
@@ -0,0 +1,9 @@
+const iconLinks = [
+  { path: '#', icon: '/src/assets/searchIcon.svg', alt: 'searchIcon' },
+  { path: '#', icon: '/src/assets/favIcon.svg', alt: 'favIcon' },
+  { path: '#', icon: '/src/assets/cartIcon.svg', alt: 'cartIcon' },
+  { path: '#', icon: '/src/assets/navLine_separator.svg', alt: 'navLine_separator' },
+  { path: '#', icon: '/src/assets/userIcon.svg', alt: 'userIcon' },
+];
+
+export default iconLinks;
