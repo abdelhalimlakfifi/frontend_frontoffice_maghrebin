c33b1f51f8b23e5136d3c23be412fef568596744
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-15 23:15:47 +0100

    update navbar component + responsive

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,54 @@
 import { Link } from "react-router-dom";
 import MyLogo from "/Icons/Maghrebin_logo.svg";
 import iconLinks from "./iconLinks";
-// import links from "./links";
+
+//import icons
+import searchIcon from '../../assets/icons/searchIcon.svg'
+import favIcon from '../../assets/icons/favIcon.svg'
+import cartIcon from '../../assets/icons/cartIcon.svg'
+import navLineDivider from '../../assets/icons/navLine_divider.svg'
+import userIcon from '../../assets/icons/userIcon.svg'
+import burgerMenu from '../../assets/icons/Bugermenu.svg'
 
 function NavBar() {
+  const links = [
+    {
+      path: '/home',
+      name: 'Home',
+    },
+    {
+      path: '/men',
+      name: 'Men',
+    },
+    {
+      path: '/women',
+      name: 'Women',
+    },
+    {
+      path: '/kids',
+      name: 'Kids',
+    },
+  ]
+
+  // const iconLinks = [
+  //   { path: '#', icon: '/src/assets/Icons/searchIcon.svg', alt: 'searchIcon' },
+  //   { path: '#', icon: '/src/assets/Icons/favIcon.svg', alt: 'favIcon' },
+  //   { path: '#', icon: '/src/assets/Icons/cartIcon.svg', alt: 'cartIcon' },
+  //   { path: '#', icon: '/src/assets/Icons/navLine_divider.svg', alt:'divider' },
+  //   { path: '#', icon: '/src/assets/Icons/userIcon.svg', alt:'userIcon' },
+  // ]
   return (
-    <>
-      {/* Menu div */}
-      <nav className="md:mx-6 lg:mx-0 md:visible">
-        {/* <ul className="flex justify-between md:w-72 p-4">
+    <div className="mx-3 lg:mx-28 flex justify-between">
+      <div className="flex items-center lg:hidden ">
+          <img
+              src={burgerMenu}
+              alt='Menu'
+              className="h-6 w-8"
+            />
+      </div>
+      <div className="hidden lg:block ">
+         {/* Menu div */}
+         <ul className="flex justify-between items-center md:w-72 p-4">
           {links.map((link) => (
             <li className="w-fit hover:underline" key={link.name}>
               <Link
@@ -19,31 +59,59 @@ function NavBar() {
               </Link>
             </li>
           ))}
-        </ul> */}
-      </nav>
-
+        </ul>
+      </div>
+      
       {/* Logo div */}
-      <div className="flex justify-center items-center">
         <Link
           to="/home"
+          className=""
         >
-          <img src={MyLogo} alt="MyLogo" className="h-12 my-1" />
+          <img src={MyLogo} alt="MyLogo" className="h-12 my-1 " />
         </Link>
-      </div>
+        
 
       {/* Icons div */}
-      {/* <div className="flex justify-center items-center space-x-3  md:mx-6">
-        {iconLinks.map((iconLink, index) => (
-          <Link key={index} to={iconLink.path}>
+      <div className="flex items-center lg:space-x-3  ">
+        
+          <Link to='/'>
             <img
-              src={iconLink.icon}
-              alt={iconLink.alt}
+              src={searchIcon}
+              alt='searchIcon'
               className="h-6 w-8"
             />
           </Link>
-        ))}
-      </div> */}
-    </>
+          <Link to='/'>
+            <img
+              src={favIcon}
+              alt='searchIcon'
+              className="h-6 w-8 hidden lg:block"
+            />
+          </Link>
+          <Link to='/'>
+            <img
+              src={cartIcon}
+              alt='cartIcon'
+              className="h-6 w-8 hidden lg:block"
+            />
+          </Link>
+          <Link to='/'>
+            <img
+              src={navLineDivider}
+              alt='navLineDivider'
+              className="h-6 w-8 hidden lg:block"
+            />
+          </Link>
+          <Link to='/'>
+            <img
+              src={userIcon}
+              alt='userIcon'
+              className="h-6 w-8 hidden lg:block"
+            />
+          </Link>
+      
+      </div>
+    </div>
   );
 }
 
