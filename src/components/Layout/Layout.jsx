bb63fa3a90686402a0a6f7ea93578855fb632ab4
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-15 23:45:32 +0100

    build the bottom navbar component

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import NavBar from '../NavBar/NavBar'
 import Footer from '../Footer/Footer'
 
 
+
+const iconLinks = [
+    { path: '#', icon: '/src/assets/Icons/HomeIcon.svg', alt:'HomeIcon' },
+    { path: '#', icon: '/src/assets/Icons/favIcon.svg', alt: 'favIcon' },
+    { path: '#', icon: '/src/assets/Icons/cartIcon.svg', alt:'cartIcon' },
+    { path: '#', icon: '/src/assets/Icons/userIcon.svg', alt: 'userIcon' },
+  ]
 
 function Layout() {
   return (
@@ -14,9 +22,13 @@ function Layout() {
         <div className='w-full h-auto border-blackV border-t-2 '>
         <Footer/>
         </div>
-        {/* <div className="lg:hidden bg-red-900 h-12 md:h-11 fixed bottom-0 left-0 w-full">
-
-        </div> */}
+        <div className="flex justify-between items-center  lg:hidden h-12 px-4 md:px-7 md:h-11 fixed bottom-0 left-0 w-full bg-white  border-blackV border-t-2  ">
+        {iconLinks.map((iconLink, index) => (
+          <Link key={index} to={iconLink.path} className="w-8">
+            <img src={iconLink.icon} alt={iconLink.alt} className="h-6 md:h-7 m-0 p-0" />
+          </Link>
+        ))}
+        </div>
 
     </div>
     
