9b250810483a12a41d016c11d5f768d9ec701281
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-16 13:31:49 +0100

    add Layout to SizeGuide component

--- src/Pages/SizeGuidePage.jsx
+++ src/Pages/SizeGuidePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import SizeGuide from "../components/SizeGuide/SizeGuide";
+import Layout from '../components/Layout/Layout';
+
 
 const SizeGuidePage = () => {
   return (
-    <>
+    <Layout>
     <SizeGuide />
-    </>
+    </Layout>
   )
 }
 
