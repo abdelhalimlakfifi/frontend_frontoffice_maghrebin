a7feabac2fc1d831c5faf4469299e91c400d5a84
Oussama kersit <kersiit@gmail.com>
2023-11-16 12:49:22 +0100

    import ProductSwiper component

--- src/components/ProductDetail/ProductImage.jsx
+++ src/components/ProductDetail/ProductImage.jsx
@@ -1,14 +1,11 @@
 import React from "react";
+import ProductsSwiper from "./ProductsSwiper";
 
 const ProductImage = () => {
   return (
     <div className="w-full lg:flex hidden">
-      <div className="border mr-4 w-9/12">Content 1 (70%)</div>
-      <div className="flex flex-col h-full w-3/12">
-        <div className="border mb-4 h-full">Column 1</div>
-        <div className="border mb-4 h-full">Column 2</div>
-        <div className="border h-full ">Column 3</div>
-      </div>
+      <div className="border mr-3 w-9/12"><img src="https://via.placeholder.com/552x616" alt="" /></div>
+      <ProductsSwiper />
     </div>
   );
 };
