a2205a4beff36d3b9703e7d5a94e4b06e9b90146
Oussama kersit <kersiit@gmail.com>
2023-11-18 21:58:56 +0100

    changing style

--- src/components/CategorieComponents/CategorieDiv.jsx
+++ src/components/CategorieComponents/CategorieDiv.jsx
@@ -4,9 +4,10 @@ import BtnGlobal from "../GlobalComponents/BtnGlobal"
 const CategorieDiv = ({ imgSrc }) => {
   return (
     <div className="w-1/3 border border-black border-solid relative">
+      <div className="absolute inset-0 bg-black opacity-20 noisy-overlay"></div>
       <img className="w-full h-full object-cover" src={imgSrc} alt="Image" />
       <BtnGlobal
-        className="absolute bottom-20 left-1/2 transform -translate-x-1/2 bg-transparent border border-black p-2"
+        className="absolute bottom-20 left-1/2 transform -translate-x-1/2 bg-white border p-4 uppercase font-NewYork tracking-widest"
         content="View More"
       />
     </div>
