9cc733c8f666653c7ba299a76c4862941b817423
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-18 01:05:57 +0100

    build women cards for landing page

--- src/components/LandingComponents/SectionTwo/Cards.jsx
+++ src/components/LandingComponents/SectionTwo/Cards.jsx
@@ -1,33 +1,53 @@
-import React from 'react'
-import BtnGlobal from "../../GlobalComponents/BtnGlobal"
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import { FreeMode, Pagination } from "swiper/modules";
+//import card image
+import Kaftan1 from "./../../../assets/Kaftan1.jpg";
+import { Card } from "./Card";
 
-const Cards = ({ imageUrl, altText, price, buttonText }) => {
+const Cards = () => {
+  // Example data for three cards
+  const cardData = [
+    { title: "Product 1", price: "20.00$", imageSrc: Kaftan1 },
+    { title: "Product 2", price: "25.00$", imageSrc: Kaftan1 },
+    { title: "Product 3", price: "30.00$", imageSrc: Kaftan1 },
+    { title: "Product 4", price: "20.00$", imageSrc: Kaftan1 },
+  ];
+
   return (
-   <>
-   <div className="Cards flex flex-col" style={{ width: "320px" }}>
-        <div>
-          <img
-            className="w-full"
-            style={{ height: "350px" }}
-            src={imageUrl}
-            alt={altText}
-          />
-        </div>
-        {/* bg-gray-800 bg-opacity-75 p-2 text-white  */}
-        <div className="flex justify-between p-2">
-          <p>View Details</p>
-          <p>Price: {price}</p>
-        </div>
-        <div>
-        {/* {buttonText} */}
-          <BtnGlobal
-            className="w-full border border-gray-500 p-2"
-            content="View Detail"
-          />
-        </div>
-      </div>
-   </>
-  )
-}
+    <>
+      {/* Show swiper in mobile */}
+      {/* <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        freeMode={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[FreeMode, Pagination]}
+        className="mySwiper lg:hidden "
+
+      >
+        <section className="flex flex-row flex-wrap justify-center mx-10">
+          {cardData.map((card, index) => (
+            <SwiperSlide key={index}>
+              <Card {...card} />
+            </SwiperSlide>
+          ))}
+        </section>
+      </Swiper> */}
+      
+      {/* Show in Web without swiper */}
+      <section className="flex flex-row flex-wrap justify-center mx-10">
+        {cardData.map((card, index) => (
+          <Card key={index} {...card} />
+        ))}
+      </section>
+    </>
+  );
+};
 
 export default Cards;
