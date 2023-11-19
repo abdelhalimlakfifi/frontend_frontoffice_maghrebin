af71fc3f96b2fffc42121bbe17f5d8b5d88471c4
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-19 14:46:26 +0100

    Create Women Components

--- src/components/LandingComponents/SectionTwo/SectionWomen/Women.jsx
+++ src/components/LandingComponents/SectionTwo/SectionWomen/Women.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Cards from "../Cards";
+import WomenCards from "./WomenCards";
 
 const Women = () => {
   return (
     <div className="flex flex-col">
         <h1 className="text-3xl font-medium flex justify-center my-5">New In Women Wear</h1>
-        <Cards />
+        <WomenCards />
     </div>
     
   );
