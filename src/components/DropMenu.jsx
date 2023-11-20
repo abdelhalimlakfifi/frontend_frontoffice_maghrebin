be314dd739c3315a60778cdb8338b253d1941ffc
Oussama kersit <kersiit@gmail.com>
2023-11-20 21:45:54 +0100

    New dropMenu component

---
+++ src/components/DropMenu.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { Dropdown } from 'primereact/dropdown';
+import { Checkbox } from 'primereact/checkbox';
+
+import '../DropMenuStyle.css';
+
+export default function DropMenu() {
+  const [selectedSize, setSelectedSize] = useState(null);
+  const [selectedColor, setSelectedColor] = useState(null);
+  const [selectedFilter, setSelectedFilter] = useState(null);
+
+  const [checked, setChecked] = useState(false);
+
+  const Sizes = [{ name: 'S' }, { name: 'M' }, { name: 'L' }, { name: 'XL' }, { name: 'XXL' }];
+  const Colors = [{ name: 'Blue' }, { name: 'Cream' }, { name: 'Light-blue' }, { name: 'Lime' }];
+  const Filter = [{ name: 'Men' }, { name: 'Women' }, { name: 'Kids' }];
+
+  const customItemTemplate = option => {
+    return (
+      <div className="flex items-center">
+        <Checkbox onChange={e => setChecked(e.checked)} checked={checked}></Checkbox>
+        <span className="ml-2">{option.name}</span>
+      </div>
+    );
+  };
+
+  return (
+    <div className="flex justify-around items-center h-[50px] w-full border-y-[1px] border-blackV">
+      <section className="flex justify-start tracking-widest text-[#2B2B2B]">
+        <Dropdown
+          value={selectedSize}
+          onChange={e => setSelectedSize(e.value)}
+          options={Sizes}
+          optionLabel="name"
+          placeholder="Size"
+          className="w-26 md:w-10rem uppercase"
+          itemTemplate={customItemTemplate}
+        />
+
+        <Dropdown
+          value={selectedColor}
+          onChange={e => setSelectedColor(e.value)}
+          options={Colors}
+          optionLabel="name"
+          placeholder="Color"
+          className="w-26 mx-6 md:w-10rem uppercase"
+          itemTemplate={customItemTemplate}
+        />
+      </section>
+
+      <section className="tracking-widest text-[#2B2B2B]">
+        <Dropdown
+          value={selectedFilter}
+          onChange={e => setSelectedFilter(e.value)}
+          options={Filter}
+          optionLabel="name"
+          placeholder="Filter"
+          className="w-26 md:w-10rem uppercase text-sm"
+          itemTemplate={customItemTemplate}
+        />
+      </section>
+    </div>
+  );
+}
