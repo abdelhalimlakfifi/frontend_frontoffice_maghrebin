2645d05feaf224eda41ab13c0b70263d6e4eab3c
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-22 00:00:22 +0100

    move productSwiper component

--- src/components/ProductDetail/ProductImage.jsx
+++ src/components/ProductDetail/ProductImage.jsx
@@ -1,14 +1,40 @@
 import React from "react";
-import ProductsSwiper from "./ProductsSwiper";
+import Takchita1 from "../../assets/Kaftan1.jpg"
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Mousewheel } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import Takchita from "../../assets/Kaftan2.jpg";
+
+const imageSources = [Takchita, Takchita, Takchita, Takchita, Takchita, Takchita, Takchita, Takchita, Takchita];
 
 const ProductImage = () => {
   return (
-    <div className="w-full lg:flex flex lg:order-2">
-      <div className="border mr-3 lg:w-9/12">
-        <img src="https://via.placeholder.com/552x616" alt="" />
+    <div className="w-full lg:flex flex lg:order-2 h-[45rem]">
+      <div className="border-2 border-black  mr-3 ">
+        <img src={Takchita1} alt=""  className="mainImg w-full h-full object-cover"/>
       </div>
-
-      <ProductsSwiper />
+      <div className=''>
+      <Swiper
+        direction={'vertical'}
+        slidesPerView={3}
+        spaceBetween={10}
+        mousewheel={true}
+        modules={[Mousewheel]}
+        className="w-full h-full"
+      >
+        {imageSources.map((src, index) => (
+          <SwiperSlide key={index} className='border-2 border-black'>
+            <img
+              src={src}
+              alt={`maghrebin-${index}`}
+              className='w-full h-full object-cover hover:opacity-60 transition-opacity duration-300 ease-in-out cursor-pointer'
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
     </div>
   );
 };
