f1ad93ba0688e21cbf70f1e3a79097b0067f2815
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-22 00:01:14 +0100

    update label to women

--- src/components/ProductDetail/MainProductDetail.jsx
+++ src/components/ProductDetail/MainProductDetail.jsx
@@ -2,24 +2,24 @@ import React from "react";
 import Product from "./Product";
 import ProductImage from "./ProductImage";
 import BreadCrum from "../GlobalComponents/BreadCrum";
-import ProductsSwiper from "./ProductsSwiper";
 import Layout from "../Layout/Layout";
 
 const MainProductDetail = () => {
   // BreadCrum
-  const items = [{ label: "Men" }, { label: "Djelaba" }];
+  const items = [{ label: "Women" }, { label: "Djelaba" }];
   return (
     // test data
     <Layout>
       <div className="mb-6 lg:px-20 px-12 mt-8">
         <BreadCrum items={items} />
       </div>
-      <section className="w-full lg:px-20 px-12 flex lg:flex-row lg:gap-12 flex-col justify-between">
+      <section className="w-full lg:px-20 px-12 flex lg:flex-row lg:gap-12 flex-col justify-between lg:my-20 ">
         <Product
           title="Product Title"
           price="$00.00"
           description="Lorem ipsum refers to a placeholder text commonly used in the publishing and graphic design industries. It's a string of Latin-looking words that have no actual meaning. "
           color="Blue"
+          
         />
         <ProductImage />
       </section>
