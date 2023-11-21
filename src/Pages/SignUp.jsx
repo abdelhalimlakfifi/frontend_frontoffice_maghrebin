d58ff25676de54ae39918187427cc68ec2c19b60
Oussama kersit <kersiit@gmail.com>
2023-11-21 20:35:43 +0100

    wrapping Signup inside Layout

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import FormSign from "../components/SignUpComponents/FormSign";
+import Layout from "../components/Layout/Layout";
 
 const SignUp = () => {
   return (
-    <>
+    <Layout>
       <FormSign />
-    </>
+    </Layout>
   );
 };
 
