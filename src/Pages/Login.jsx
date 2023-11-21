ab07f2034bc89d32d2e416df590390cf75fb7260
Oussama kersit <kersiit@gmail.com>
2023-11-21 20:36:00 +0100

    wrapping login inside Layout

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import FormLogin from "../components/LoginComponents/FormLogin";
+import Layout from "../components/Layout/Layout";
 
 function Login() {
   return (
-    <>
+    <Layout>
       <FormLogin />
-    </>
+    </Layout>
   );
 }
 
