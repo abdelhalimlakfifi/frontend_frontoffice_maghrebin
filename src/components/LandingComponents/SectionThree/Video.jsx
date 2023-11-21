acb49b196515fa914b61e38c4c30e02f5abb4f0f
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-22 00:01:52 +0100

    update style

--- src/components/LandingComponents/SectionThree/Video.jsx
+++ src/components/LandingComponents/SectionThree/Video.jsx
@@ -3,7 +3,7 @@ import TrailerMaghrebin from "../../../assets/MoroccoArise.mp4"
 
 const Video = () => {
   return (
-    <section className="  w-full border-t-2 border-b-2 border-black flex justify-center items-center h-auto  ">
+    <section className=" w-full border-t-2 border-b-2 border-black flex justify-center items-center h-auto  ">
       <video controls className="w-full" >
         <source src={TrailerMaghrebin} type="video/mp4" />
         Sorry, your browser doesn't support videos.
