22f3435b588798d97c32212cf5c93b87598c2a81
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-22 22:26:32 +0100

    update CategorieDiv style

--- src/components/CategorieComponents/CategorieDiv.jsx
+++ src/components/CategorieComponents/CategorieDiv.jsx
@@ -3,10 +3,10 @@ import BtnGlobal from "../GlobalComponents/BtnGlobal"
 
 const CategorieDiv = ({ imgSrc }) => {
   return (
-    <div className="w-1/3 border border-black border-solid relative">
-      <img className="w-full h-full object-cover" src={imgSrc} alt="Image" />
+    <div className="w-1/3 border-r border-2 border-blackV relative">
+      <img className="w-full h-full object-cover hover:opacity-80 transition-opacity duration-300 ease-in-out cursor-pointer" src={imgSrc} alt="Image" />
       <BtnGlobal
-        className="absolute bottom-20 left-1/2 transform -translate-x-1/2 bg-transparent border border-black p-2"
+        className="absolute bottom-10 border-2 left-1/2 p-5 w-52 transform -translate-x-1/2 bg-white hover:bg-blackV  border-blackV hover:border-white hover:text-white  font-medium text-lg "
         content="View More"
       />
     </div>
