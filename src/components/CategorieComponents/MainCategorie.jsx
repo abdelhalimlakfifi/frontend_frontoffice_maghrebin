acf3ec807c300655eae6e3e8e30b42bc02f26067
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-22 22:26:47 +0100

    add mainImages

--- src/components/CategorieComponents/MainCategorie.jsx
+++ src/components/CategorieComponents/MainCategorie.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import CategorieDiv from "./CategorieDiv";
+import MainMen from '../../assets/MenMain2.jpg'
+import MainKid from '../../assets/Kidmain2.jpg'
+import MainWomen from '../../assets/WomenMain.webp'
 
 const MainCategorie = () => {
   return (
-    <section className="w-full flex" style={{ height: "700px" }}>
-      <CategorieDiv imgSrc="LandingPage/Jelalebs/jelab.jpg" />
-      <CategorieDiv imgSrc="your_image_url_2" />
-      <CategorieDiv imgSrc="your_image_url_3" />
+    <section className="w-full flex h-[46rem]" >
+      <CategorieDiv imgSrc={MainMen} />
+      <CategorieDiv imgSrc={MainWomen} />
+      <CategorieDiv imgSrc={MainKid} />
     </section>
   );
 };
