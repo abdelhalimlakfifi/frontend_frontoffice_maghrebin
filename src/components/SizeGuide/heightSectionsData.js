0ffadb0a52a8f2c58d5e3421b84b35632f40ad0c
Oussama kersit <kersiit@gmail.com>
2023-11-22 23:11:21 +0100

    refactor code

--- src/components/SizeGuide/heightSectionsData.js
+++ src/components/SizeGuide/heightSectionsData.js
@@ -1,7 +1,7 @@
 const heightSections = [
   { title: "1 - Height", content: "Without shoes, from the top of the head to the soles of the feet (for assistance, a door frame is perfect)." },
   { title: "2 - Chest circumference", content: "Measured horizontally at the fullest part of the chest." },
-  { title: "2a - Underbust circumference", content: "Position the tape measure just below the chest horizontally." },
+  { title: "2A - Underbust circumference", content: "Position the tape measure just below the chest horizontally." },
   { title: "3 - Waist circumference", content: "Measure around the waist without tightening." },
   { title: "4 - Hip circumference", content: "Measure horizontally at the fullest part of the hips." },
   { title: "5 - Inseam length", content: "Along the leg, starting from the top of the inner thighs to the bottom of the feet." }
