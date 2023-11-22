4908c46f649325bbe237a30945afd54dd0ddcd2e
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-23 00:47:09 +0100

    make CategorieDiv responsive

--- src/components/CategorieComponents/CategorieDiv.jsx
+++ src/components/CategorieComponents/CategorieDiv.jsx
@@ -4,9 +4,9 @@ import BtnGlobal from "../GlobalComponents/BtnGlobal"
 const CategorieDiv = ({ imgSrc }) => {
   return (
     <div className="w-1/3 border-r border-2 border-blackV relative">
-      <img className="w-full h-full object-cover hover:opacity-80 transition-opacity duration-300 ease-in-out cursor-pointer" src={imgSrc} alt="Image" />
+      <img className="w-full h-full object-cover hover:opacity-90 transition-opacity duration-300 ease-in-out cursor-pointer" src={imgSrc} alt="Image" />
       <BtnGlobal
-        className="absolute bottom-10 border-2 left-1/2 p-5 w-52 transform -translate-x-1/2 bg-white hover:bg-blackV  border-blackV hover:border-white hover:text-white  font-medium text-lg "
+        className="absolute bottom-10 border-2 left-1/2 p-3 w-40 md:p-4 md:w-44  lg:p-5 lg:w-52 transform -translate-x-1/2 bg-white hover:bg-purple-400 border-blackV hover:border-white hover:text-white  font-medium text-lg "
         content="View More"
       />
     </div>
