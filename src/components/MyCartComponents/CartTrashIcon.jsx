8c46a2f130ae1c89a256853142acb1825b02385c
Oussama kersit <kersiit@gmail.com>
2023-11-23 10:00:50 +0100

    fixing responsive

--- src/components/MyCartComponents/CartTrashIcon.jsx
+++ src/components/MyCartComponents/CartTrashIcon.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CartTrashIcon = () => {
   return (
    <>
-     <div className="w-2/5 flex justify-end">
+     <div className="w-1/5 mt-1 lg:mt-0 lg:w-2/5 flex justify-end">
           <i className="pi pi-trash text-xl cursor-pointer"></i>
         </div>
    </>
