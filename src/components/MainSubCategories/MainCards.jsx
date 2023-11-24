b77ae4a80e8453f9e223b7f629732e756c2fac4e
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-24 21:50:33 +0100

    make it responsive

--- src/components/MainSubCategories/MainCards.jsx
+++ src/components/MainSubCategories/MainCards.jsx
@@ -8,59 +8,62 @@ import wedding from "../../assets/wedding.jpg";
 
 function MainCards() {
   return (
-    <div className=" w-full h-full">
-      <h1 className=" text-3xl font-medium flex justify-center font-NewYork text-blackV mt-11 mb-5 h-auto">
-        Explore Moroccan Traditions
-      </h1>
-      <div className="flex flex-col mb-11 h-full w-full">
-        <div className="mx-11 md:mx-11 lg:mx-52 xl:mx-80 flex justify-between h-[25rem] md:h-[35rem] lg:h-[45rem] ">
-          <div className=" relative w-[15rem] md:w-[20rem] lg:w-[30rem] xl:w-[38rem]">
-            <img
-              className="w-full h-full object-cover hover:opacity-90 transition-opacity duration-300 ease-in-out cursor-pointer"
-              src={SubCategoryImg}
-              alt="Image"
-            />
-            <BtnGlobal
-              className="absolute bottom-10 border-2 left-1/2 p-2 w-40 md:p-3 md:w-72 lg:p-4 lg:w-96 transform -translate-x-1/2 bg-white hover:bg-blackV border-blackV hover:border-white hover:text-white   font-medium text-lg "
-              content="Amazigh"
-            />
-          </div>
-          <div className="  relative w-[15rem]   md:w-[20rem] lg:w-[30rem] xl:w-[38rem]">
-            <img
-              className="w-full h-full object-cover hover:opacity-90 transition-opacity duration-300 ease-in-out cursor-pointer"
-              src={sahara}
-              alt="Image"
-            />
-            <BtnGlobal
-              className="absolute bottom-10 border-2 left-1/2 p-2 w-40 md:p-3 md:w-72 lg:p-4 lg:w-96 transform -translate-x-1/2 bg-white hover:bg-blackV border-blackV hover:border-white hover:text-white  font-medium text-lg "
-              content="Sahraoui"
-            />
-          </div>
-        </div>
+    <div className="w-full flex justify-center">
+      <div className=" w-full max-w-7xl h-full">
+        <h1 className=" text-3xl font-medium flex justify-center font-NewYork text-blackV mt-11 mb-5 h-auto">
+          Explore Moroccan Traditions
+        </h1>
+        <div className="flex flex-col mb-11 h-full w-full  font-DIN ">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 mx-5 md:mx-11 lg:mx-10 place-items-center">
+          {/* w-[15rem] md:w-[20rem] lg:w-[30rem] xl:w-[38rem] */}
+              
+            <div className="relative lg:w-full w-full md:w-[80%] h-[35rem] md:h-[40rem]  lg:h-[38rem] xl:h-[48rem]">
+              <img
+                className="w-full h-full object-cover hover:opacity-90 transition-opacity duration-300 ease-in-out cursor-pointer"
+                src={SubCategoryImg}
+                alt="Image"
+              />
+              <BtnGlobal
+                className="absolute bottom-10 border-2 left-1/2 p-3 w-56 md:p-5 md:w-80 lg:p-4 lg:w-96 transform -translate-x-1/2 bg-white hover:bg-blackV border-blackV hover:border-white hover:text-white   font-medium text-xl "
+                content="Amazigh"
+              />
+            </div>
+            <div className="relative lg:w-full w-full md:w-[80%] h-[35rem] md:h-[40rem]   lg:h-[38rem] xl:h-[48rem]">
+              <img
+                className="w-full h-full object-cover hover:opacity-90 transition-opacity duration-300 ease-in-out cursor-pointer"
+                src={sahara}
+                alt="Image"
+              />
+              <BtnGlobal
+                className="absolute bottom-10 border-2 left-1/2 p-3 w-56 md:p-5 md:w-80  lg:p-4 lg:w-96 transform -translate-x-1/2 bg-white hover:bg-blackV border-blackV hover:border-white hover:text-white  font-medium text-xl "
+                content="Sahraoui"
+              />
+            </div>
+            <div className="relative lg:w-full w-full md:w-[80%] h-[35rem] md:h-[40rem]  lg:h-[38rem] xl:h-[48rem]">
+              <img
+                className="w-full h-full object-cover hover:opacity-90 transition-opacity duration-300 ease-in-out cursor-pointer"
+                src={wedding}
+                alt="Image"
+              />
+              <BtnGlobal
+                className="absolute bottom-10 border-2 left-1/2 p-3 w-56 md:p-5 md:w-80  lg:p-4 lg:w-96 transform -translate-x-1/2 bg-white hover:bg-blackV border-blackV hover:border-white hover:text-white  font-medium text-xl "
+                content="Wedding"
+              />
+            </div>
+            <div className="relative lg:w-full w-full md:w-[80%] h-[35rem] md:h-[40rem]  lg:h-[38rem] xl:h-[48rem]">
+              <img
+                className="w-full h-full object-cover hover:opacity-90 transition-opacity duration-300 ease-in-out cursor-pointer"
+                src={Litem}
+                alt="Image"
+              />
+              <BtnGlobal
+                className="absolute bottom-10 border-2 left-1/2 p-3 w-56 md:p-5 md:w-80  lg:p-4 lg:w-96transform -translate-x-1/2 bg-white hover:bg-blackV border-blackV hover:border-white hover:text-white  font-medium text-xl "
+                content="E-litem  "
+              />
+            </div>
 
-        <div className="mx-11 mt-11 lg:mt-20 md:mx-11 lg:mx-52 xl:mx-80 flex justify-between h-[25rem] md:h-[35rem] lg:h-[45rem] ">
-          <div className=" relative w-[15rem]  md:w-[20rem] lg:w-[30rem] xl:w-[38rem] ">
-            <img
-              className="w-full h-full object-cover hover:opacity-90 transition-opacity duration-300 ease-in-out cursor-pointer"
-              src={wedding}
-              alt="Image"
-            />
-            <BtnGlobal
-              className="absolute bottom-10 border-2 left-1/2 p-2 w-40 md:p-3 md:w-72 lg:p-4 lg:w-96 transform -translate-x-1/2 bg-white hover:bg-blackV border-blackV hover:border-white hover:text-white  font-medium text-lg "
-              content="Wedding"
-            />
           </div>
-          <div className=" relative w-[15rem]   md:w-[20rem] lg:w-[30rem] xl:w-[38rem]">
-            <img
-              className="w-full h-full object-cover hover:opacity-90 transition-opacity duration-300 ease-in-out cursor-pointer"
-              src={Litem}
-              alt="Image"
-            />
-            <BtnGlobal
-              className="absolute bottom-10 border-2 left-1/2 p-2 w-40 md:p-3 md:w-72 lg:p-4 lg:w-96transform -translate-x-1/2 bg-white hover:bg-blackV border-blackV hover:border-white hover:text-white  font-medium text-lg "
-              content="E-litem  "
-            />
-          </div>
+
         </div>
       </div>
     </div>
--- src/components/OurStory/OurStory.jsx
+++ src/components/OurStory/OurStory.jsx
@@ -4,21 +4,21 @@ import FullLogo from "../../assets/FullLogo.svg";
 
 function OurStory() {
   return (
-    <div className="w-full h-[50rem]  md:h-[40rem] lg:h-[50rem]  xl:h-[60rem] flex items-center">
+    <div className="w-full h-[70rem]  md:h-[64rem] lg:h-[50rem]  xl:h-[54rem] flex items-center ">
       <div className="w-full h-3/4 border-t-2 border-b-2 border-blackV relative flex justify-center items-center">
        
-        <img src={FullLogo} alt="Maghrebin" className="bg-white w-48 px-5 absolute -top-5 left-7  md:left-10 lg:left-40 flex justify-center" />
+        <img src={FullLogo} alt="Maghrebin" className="bg-white w-48 px-5 absolute -top-5 left-32    md:left-72 lg:left-14 xl:left-40 flex justify-center" />
 
-        <div className=" w-[87%] lg:w-[90%] xl:w-[66%] h-[90%] lg:h-[85%] lx:h-[85%] flex justify-center space-x-10 ">
-          <div className=" h-full w-1/2 flex flex-col items-start justify-between">
+        <div className="w-[95%] md:w-[87%] lg:w-[90%] xl:w-[66%]  h-[85%] lg:h-[79%] lx:h-[85%] flex flex-col-reverse lg:flex-row items-center justify-center lg:space-x-10  ">
+          <div className=" h-full w-full lg:w-1/2 flex flex-col items-center lg:items-start  lg:justify-between">
           
-            <div className="w-[80%] lg:py-5 xl:py-11 ">
+            <div className="w-full lg:w-[80%] p-3 lg:p-0 flex justify-center lg:justify-start">
            
-            <h1 className="lg:text-6xl  xl:text-8xl font-medium font-NewYork">Our Story</h1>
+            <h1 className="text-3xl md:text-4xl lg:text-5xl  xl:text-7xl font-medium font-NewYork ">Our Story</h1>
 
             </div>
             
-            <div className="text-base font-medium font-DIN lg:h-[80%] xl:h-[70%] flex flex-col lg:pr-2 lx:py-5 lx:pr-14 text-blackV">
+            <div className="text-base font-medium font-DIN   md:h-[80%] flex flex-col lg:pr-2 lx:py-5 lx:pr-14 text-blackV ">
               <p className=" text-justify">
                 We are <b>Maghrebin</b>, your ultimate destination for
                 exquisite traditional Moroccan clothing. At <b>Maghrebin</b>, we
@@ -29,7 +29,7 @@ function OurStory() {
                 experience the elegance and beauty of traditional Moroccan
                 fashion.
               </p>
-              <p className="mt-11 text-justify">
+              <p className="mt-4 md:mt-5 lg:mt-9 xl:mt-9 text-justify">
                 We understand the value of traditional craftsmanship and the
                 importance of sustaining local communities. That's why we
                 collaborate with talented artisans, known as <b>Maalam</b> who pour
@@ -41,7 +41,7 @@ function OurStory() {
               
             </div>
           </div>
-          <div className=" h-full w-1/2">
+          <div className="h-[45%] md:h-[55%] lg:h-[32rem] w-[100%]  lg:w-1/2 ">
             <img
               src={OurStoryImg}
               alt="maghrebin"
