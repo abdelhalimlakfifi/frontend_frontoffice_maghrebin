b025f7af05f0233d99354b5b23ca8858789574aa
Oussama kersit <kersiit@gmail.com>
2023-11-28 12:39:05 +0100

    items count function

--- src/components/MyCartComponents/CartTitle.jsx
+++ src/components/MyCartComponents/CartTitle.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from 'react';
 
 const CartTitle = () => {
+  // State to track the number of items in the cart
+  const [itemCount, setItemCount] = useState(1); // Set an initial count (e.g., 3 items)
+
+  // Function to add an item to the cart
+  const addItemToCart = () => {
+    setItemCount(itemCount + 1);
+  };
+
   return (
     <>
-      <div className="flex justify-between border-b-2 border-gray-500">
-        <h2 className="text-lg font-medium">PERSONAL INFORMATION</h2>
-        <p className="text-lg font-medium">ITEMS</p>
+      <div className="flex justify-between items-center border-b-2 border-gray-500">
+        <h2 className="text-lg font-medium">MY CART</h2>
+        <div className="flex items-center">
+          <p className="text-sm font-medium">
+            {`${itemCount} `}<span className="ml-1">ITEMS</span>
+          </p>
+        </div>
       </div>
     </>
   );
 };
 
-export  default CartTitle;
+export default CartTitle;
