ef366baadf2636d494e25d4c43230578752389a3
Oussama kersit <kersiit@gmail.com>
2023-11-28 12:41:05 +0100

    dd black logo insted of white one, editing style, slider duration

--- src/components/LandingComponents/SectionThree/LogoSlider.jsx
+++ src/components/LandingComponents/SectionThree/LogoSlider.jsx
@@ -1,53 +1,37 @@
-import React from "react";
-import Slider from 'react-infinite-logo-slider'
+import React from 'react';
+import Slider from 'react-infinite-logo-slider';
 
-import Zelija from "../../../assets/Zelija-Slider.svg"
-import Logo from "../../../assets/icons/Logo-Slider.svg"
+import Zelija from '../../../assets/Zelija-Slider.svg';
+import Logo from '../../../assets/icons/LogoSlider-01.svg';
 
 const LogoSlider = () => {
-    
-    return (
-        <div className="my-9 h-[60px] flex justify-center items-center w-full relative overflow-hidden bg-blackV">
-            <Slider
-            duration={25}
-            toRight={false}
-            pauseOnHover={false}
-            className="flex justify-center items-center"
-            >
+  return (
+    <div className="my-9 h-[60px] flex justify-center items-center w-full relative overflow-hidden bg-userProfile">
+      <Slider duration={20} toRight={false} pauseOnHover={false} className="flex justify-center items-center">
+        <Slider.Slide className="flex items-center justify-center">
+          <img src={Zelija} alt="Maghrebin" className="h-10" />
+        </Slider.Slide>
 
+        <Slider.Slide className="flex items-center justify-center">
+          <img src={Logo} alt="Maghrebin" className="h-10" />
+        </Slider.Slide>
+        <Slider.Slide className="flex items-center justify-center">
+          <img src={Zelija} alt="Maghrebin" className="h-10" />
+        </Slider.Slide>
 
-            <Slider.Slide className='flex items-center justify-center' >
-                <img src={Zelija} alt="Maghrebin" className="h-10" />
-            </Slider.Slide>
+        <Slider.Slide className="flex items-center justify-center">
+          <img src={Logo} alt="Maghrebin" className="h-10" />
+        </Slider.Slide>
+        <Slider.Slide className="flex items-center justify-center">
+          <img src={Zelija} alt="Maghrebin" className="h-10" />
+        </Slider.Slide>
 
+        <Slider.Slide className="flex items-center justify-center">
+          <img src={Logo} alt="Maghrebin" className="h-10" />
+        </Slider.Slide>
+      </Slider>
+    </div>
+  );
+};
 
-            <Slider.Slide className='flex items-center justify-center'>
-                <img src={Logo}  alt="Maghrebin" className="h-10" />
-            </Slider.Slide>
-            <Slider.Slide className='flex items-center justify-center' >
-                <img src={Zelija} alt="Maghrebin" className="h-10" />
-            </Slider.Slide>
-
-
-            <Slider.Slide className='flex items-center justify-center'>
-                <img src={Logo}  alt="Maghrebin" className="h-10" />
-            </Slider.Slide>
-            <Slider.Slide className='flex items-center justify-center' >
-                <img src={Zelija} alt="Maghrebin" className="h-10" />
-            </Slider.Slide>
-
-
-            <Slider.Slide className='flex items-center justify-center'>
-                <img src={Logo}  alt="Maghrebin" className="h-10" />
-            </Slider.Slide>
-          
-
-           
-        </Slider>
-        </div>
-        
-        
-    )
-}              
-                     
-export default LogoSlider
+export default LogoSlider;
