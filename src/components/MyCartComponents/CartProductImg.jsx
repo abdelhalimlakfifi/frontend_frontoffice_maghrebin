b67e58453c01ff897ebb69b6b926f4a0ab49ead0
Oussama kersit <kersiit@gmail.com>
2023-11-28 12:39:20 +0100

    editing width

--- src/components/MyCartComponents/CartProductImg.jsx
+++ src/components/MyCartComponents/CartProductImg.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const CartProductImg = () => {
   return (
-   <>
-    <div className="w-3/5 md:w-2/5 md:mr-8 mr-5 lg:w-1/5 lg:mr-8">
+  <>
+    <div className="w-2/5 h-2/5 md:w-2/5 md:mr-8 mr-5 lg:w-1/5 lg:mr-8">
           <img
             src="https://via.placeholder.com/208x240"
             alt="Product"
             className="w-full h-full object-cover"
           />
         </div>
-   </>
+  </>
   )
 }
 
