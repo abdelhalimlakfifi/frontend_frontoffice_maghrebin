fcd1442a01cc8196022539126c957eb30053afca
Oussama kersit <kersiit@gmail.com>
2023-11-28 12:31:50 +0100

    commenting Layout temporarily

--- src/Pages/MyCartPage.jsx
+++ src/Pages/MyCartPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import MainCart from "../components/MyCartComponents/MainCart";
-import Layout from '../components/Layout/Layout';
+// import Layout from '../components/Layout/Layout';
 
 const MyCartPage = () => {
   return (
