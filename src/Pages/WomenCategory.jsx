5eb1a072fb9fbc65ea7a1f3d484eacc518559b02
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-29 22:27:26 +0100

    optimise code make it modular

--- src/Pages/WomenCategory.jsx
+++ src/Pages/WomenCategory.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import Layout from "../components/Layout/Layout";
-import { Sidebar } from "primereact/sidebar";
-import { Slider } from "primereact/slider";
 import { Card } from "../components/GlobalComponents/ProductCard";
+import SidebarFilter from "../components/Filter/SideBarFilter";
+import FilterBar from "../components/Filter/FilterBar";
 
 //import card image
 import Kaftan1 from "../assets/Kaftan1.jpg";
@@ -168,153 +168,30 @@ function WomenCategory() {
   return (
     <Layout>
       <div className="flex w-full h-full font-DIN ">
-        <div className="card flex justify-content-center ">
-          <Sidebar
-            visible={visible}
-            position="top"
-            onHide={() => setVisible(false)}
-            style={{ marginTop: "3rem", height: "20rem" }}
-          >
-              <div className=" flex flex-col">
-                <h1 className="font-semibold text-base  border-b text-blackV border-blackV w-full">
-                  Color
-                </h1>
-                <div className="my-3 grid grid-cols-5 place-items-center gap-3">
-                  {colorTypes.map((colorType) => (
-                    <button
-                      key={colorType}
-                      className={`${colorType} h-6 w-6 lg:h-5 lg:w-5 border-blackV border hover:shadow-amber-200 hover:shadow-xl ${
-                        activeColor === colorType ? "border-fuchsia-200" : ""
-                      } `}
-                      type={colorType}
-                      onClick={() => handleColorFilter(colorType)}
-                    ></button>
-                  ))}
-                </div>
-              </div>
+        
+        <SidebarFilter
+          visible={visible}
+          setVisible={setVisible}
+          colorTypes={colorTypes}
+          sizes={sizes}
+          activeColor={activeColor}
+          activeSize={activeSize}
+          activeStockStatus={activeStockStatus}
+          handleColorFilter={handleColorFilter}
+          handleSizeButtonClick={handleSizeButtonClick}
+          handleStockStatusFilter={handleStockStatusFilter}
+        />
 
-            <div className="my-5 flex flex-col ">
-            <h1 className="font-semibold text-base  border-b text-blackV border-blackV w-full">
-              Size
-            </h1>
-            <div className="my-3  grid grid-cols-3 place-items-center gap-4">
-              {sizes.map((size, index) => (
-                <button
-                  key={index}
-                  className={` h-9 w-20 border border-blackV hover:bg-blackV hover:text-white ${
-                    activeSize === size ? "bg-blackV text-white" : ""
-                  }`}
-                  onClick={() => handleSizeButtonClick(size)}
-                >
-                  {size}
-                </button>
-              ))}
-            </div>
-          </div>  
-
-        <div className="my-5 flex flex-col">
-            <h1 className="font-semibold text-base  border-b text-blackV border-blackV w-full">
-              Stock
-            </h1>
-            <div className="my-2 flex flex-col items-start gap-3 p-3 ">
-              {["Best Selling", "New in Stock", "Old in Stock"].map(
-                (status) => (
-                  <button
-                    key={status}
-                    className={`hover:underline ${
-                      activeStockStatus === status ? "font-bold" : ""
-                    }`}
-                    onClick={() => handleStockStatusFilter(status)}
-                  >
-                    {status}
-                  </button>
-                )
-              )}
-            </div>
-          </div>
-
-          <div className="my-5 flex flex-col">
-            <h1 className="font-semibold text-base  border-b text-blackV border-blackV w-full">
-              Price
-            </h1>
-            <div className="py-7 rounded-lg flex justify-center "></div>
-          </div>
-          </Sidebar>
-        </div>
-
-        <div
-          className={
-            " hidden md:block md:w-[25%] xl:w-[15%] border-r-2 border-blackV  md:p-5 xl:p-3"
-          }
-        >
-          <div className="mb-9 lg:mb-11">
-            <h1 className="font-extrabold text-2xl  w-full">Filter</h1>
-          </div>
-          <div className="my-9 lg:my-5 flex flex-col">
-            <h1 className="font-semibold text-base  border-b text-blackV border-blackV w-full">
-              Color
-            </h1>
-            <div className="my-3 grid grid-cols-3 lg:grid-cols-5 place-items-center gap-3">
-              {colorTypes.map((colorType) => (
-                <button
-                  key={colorType}
-                  className={`${colorType} h-6 w-6 lg:h-5 lg:w-5 border-blackV border hover:shadow-amber-200 hover:shadow-xl ${
-                    activeColor === colorType ? "border-fuchsia-200" : ""
-                  } `}
-                  type={colorType}
-                  onClick={() => handleColorFilter(colorType)}
-                ></button>
-              ))}
-            </div>
-          </div>
-
-          <div className="my-9 lg:my-5 flex flex-col">
-            <h1 className="font-semibold text-base  border-b text-blackV border-blackV w-full">
-              Size
-            </h1>
-            <div className="my-4 grid grid-cols-2 lg:grid-cols-3 place-items-center gap-4 lg:gap-3">
-              {sizes.map((size, index) => (
-                <button
-                  key={index}
-                  className={`lg:h-11 lg:w-11 h-11 w-16 border border-blackV hover:bg-blackV hover:text-white ${
-                    activeSize === size ? "bg-blackV text-white" : ""
-                  }`}
-                  onClick={() => handleSizeButtonClick(size)}
-                >
-                  {size}
-                </button>
-              ))}
-            </div>
-          </div>
-
-          <div className="my-9 lg:my-5 flex flex-col">
-            <h1 className="font-semibold text-base  border-b text-blackV border-blackV w-full">
-              Stock
-            </h1>
-            <div className="my-2 flex flex-col items-start gap-3 p-3 ">
-              {["Best Selling", "New in Stock", "Old in Stock"].map(
-                (status) => (
-                  <button
-                    key={status}
-                    className={`hover:underline ${
-                      activeStockStatus === status ? "font-bold" : ""
-                    }`}
-                    onClick={() => handleStockStatusFilter(status)}
-                  >
-                    {status}
-                  </button>
-                )
-              )}
-            </div>
-          </div>
-
-          <div className="my-9 lg:my-5 flex flex-col">
-            <h1 className="font-semibold text-base  border-b text-blackV border-blackV w-full">
-              Price
-            </h1>
-            <div className="py-7 rounded-lg flex justify-center "></div>
-          </div>
-        </div>
+        <FilterBar
+          colorTypes={colorTypes}
+          sizes={sizes}
+          activeColor={activeColor}
+          activeSize={activeSize}
+          activeStockStatus={activeStockStatus}
+          handleColorFilter={handleColorFilter}
+          handleSizeButtonClick={handleSizeButtonClick}
+          handleStockStatusFilter={handleStockStatusFilter}
+        />
 
         <div className=" w-full h-full md:w-full  ">
           <div className="m-3 md:hidden flex justify-end">
