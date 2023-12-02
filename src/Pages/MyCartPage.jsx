c54593d9ae1b3cb798b4e6e743cc447561247370
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-12-02 23:52:50 +0100

    transfer CartProductImg and CartTrashIcon

--- src/Pages/MyCartPage.jsx
+++ src/Pages/MyCartPage.jsx
@@ -1,31 +1,33 @@
 import React from "react";
-import CartTitle from "../components/MyCartComponents/CartTitle"
-import CartProductImg from "../components/MyCartComponents/CartProductImg";
+import CartTitle from "../components/MyCartComponents/CartTitle";
 import CartProductDescription from "../components/MyCartComponents/CartProductDescription";
-import CartTrashIcon from "../components/MyCartComponents/CartTrashIcon";
-// import Layout from '../components/Layout/Layout';
+import takchita from "../assets/ProductImages/jelaba/BeigeJellaba.jpg"
 
 const MyCartPage = () => {
   return (
-    <>
-     <section className="flex flex-col mx-10 md:flex-row lg:w-[90vw] py-7">
-      <div className="w-full md:px-28 md:pr-20">
-        {/* Cart Title */}
-        <CartTitle />
+    <section className="flex lg:flex-col lg:w-[20rem]  ">
+      {/* Cart Title */}
+      <CartTitle />
 
-        <div className="flex lg:block mt-8" style={{ height: "300px" }}>
-          {/* First Div with an Image */}
-          <CartProductImg />
+      <div className="flex flex-row mt-8 w-full gap-3">
+        {/* First Div with an Image */}
+        <div className="h-[9rem] ">
+          <img
+            src={takchita}
+            alt="Product"
+            className="w-full h-full object-cover"
+          />
+        </div>
 
-          {/* Second Div with Description and Button */}
-          <CartProductDescription />
+        {/* Second Div with Description and Button */}
+        <CartProductDescription />
 
-          {/* Third Div with a Trash Icon */}
-          <CartTrashIcon />
+        {/* Third Div with a Trash Icon */}
+        <div className="w-[10%] flex justify-end p-1">
+          <i className="pi pi-trash text-xl cursor-pointer"></i>
         </div>
       </div>
     </section>
-    </>
   );
 };
 
