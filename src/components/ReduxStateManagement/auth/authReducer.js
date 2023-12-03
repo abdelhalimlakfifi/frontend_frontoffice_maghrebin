d1bf97a6a57d044e083c30fb93133aef8f6a1a45
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-12-04 00:25:39 +0100

    update login reducer and create sign up reducer

--- src/components/ReduxStateManagement/auth/authReducer.js
+++ src/components/ReduxStateManagement/auth/authReducer.js
@@ -1,25 +1,47 @@
+// userSlice.js
 import { createSlice } from "@reduxjs/toolkit";
+import { loginUser, signupUser } from "./authActions";
 
-const initialState = {
-  user: "",
-  error: null,
-};
-
-const authSlice = createSlice({
-  name: "auth",
-  initialState,
-  reducers: {},
+const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    loading: false,
+    user: null,
+    error: null
+  },
   extraReducers: (builder) => {
     builder
-      .addCase(login.fulfilled, (state, action) => {
+      .addCase(loginUser.pending, (state) => {
+        state.loading = true;
+        state.user = null;
+        state.error = null;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.loading = false;
         state.user = action.payload;
         state.error = null;
       })
-      .addCase(login.rejected, (state, action) => {
+      .addCase(loginUser.rejected, (state, action) => {
+        state.loading = false;
         state.user = null;
-        state.error = action.error.message;
+        state.error = action.payload;
+      })
+      .addCase(signupUser.pending, (state) => {
+        state.loading = true;
+        state.user = null;
+        state.error = null;
+      })
+      .addCase(signupUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload;
+        state.error = null;
+      })
+      .addCase(signupUser.rejected, (state, action) => {
+        state.loading = false;
+        state.user = null;
+        state.error = action.payload;
       });
-  },
+  }
 });
 
-export default authSlice.reducer;
+export default userSlice.reducer;
