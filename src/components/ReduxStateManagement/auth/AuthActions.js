57722d89ecfd5b251be82686cfbcaddcbc8276de
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-12-04 00:25:17 +0100

    create actions for signup

--- src/components/ReduxStateManagement/auth/AuthActions.js
+++ src/components/ReduxStateManagement/auth/AuthActions.js
@@ -1,12 +1,48 @@
 // authActions.js
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import {
+    createAsyncThunk
+} from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const login = createAsyncThunk("auth/login", async ({ email, password }) => {
-  try {
-    const response = await axios.post("/api/login", { email, password });
-    return response.data;
-  } catch (error) {
-    throw new Error(error.message);
-  }
-});
+export const loginUser = createAsyncThunk(
+    '/login',
+    async (userCredentials, {
+        rejectWithValue
+    }) => {
+        try {
+            const response = await axios.post('api/login', userCredentials);
+            localStorage.setItem('user', JSON.stringify(response.data));
+            return {
+                status: response.status,
+                response
+            };
+        } catch (error) {
+            return rejectWithValue({
+                data: error.response.data,
+                status: error.response.status
+            });
+        }
+    }
+);
+
+
+export const signupUser = createAsyncThunk(
+    '/sign-up',
+    async (userData, {
+        rejectWithValue
+    }) => {
+        try {
+            const response = await axios.post('api/sign-up', userData);
+            localStorage.setItem('user', JSON.stringify(response.data));
+            return {
+                status: response.status,
+                response
+            };
+        } catch (error) {
+            return rejectWithValue({
+                data: error.response.data,
+                status: error.response.status
+            });
+        }
+    }
+);
