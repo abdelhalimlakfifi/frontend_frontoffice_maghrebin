e9d70a0e0a83b43646f3fa5d4231954f17dec7c7
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-12-04 01:39:30 +0100

    add userProfile to redux store

--- src/components/ReduxStateManagement/store.js
+++ src/components/ReduxStateManagement/store.js
@@ -2,10 +2,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
 import authReducer from "./auth/authReducer";
+import userProfileReducer from "./userProfile/userProfileActions";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    userProfile: userProfileReducer
   },
   middleware: [thunk],
 });
