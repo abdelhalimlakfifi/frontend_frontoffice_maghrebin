bb69b4c61939140eb496e4d00e2ba459ebc42843
Oussama kersit <kersiit@gmail.com>
2023-12-12 12:50:06 +0100

    Add uppercase

--- src/components/LandingComponents/SectionTwo/SectionMen/Men.jsx
+++ src/components/LandingComponents/SectionTwo/SectionMen/Men.jsx
@@ -4,7 +4,7 @@ import MenCards from "./MenCards";
 const Men = () => {
   return (
     <div className="flex flex-col font-DIN ">
-        <h1 className="text-3xl font-medium flex justify-center mt-20 font-NewYork text-blackV">New In Men Wear</h1>
+        <h1 className="uppercase text-3xl font-medium flex justify-center mt-20 font-NewYork text-blackV">New In Men Wear</h1>
         <MenCards />
     </div>
     
