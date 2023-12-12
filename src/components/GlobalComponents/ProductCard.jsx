5825436af09ff8b2a64bea917d096ed970540a0d
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-12-12 13:24:52 +0100

    refactor code

--- src/components/GlobalComponents/ProductCard.jsx
+++ src/components/GlobalComponents/ProductCard.jsx
@@ -4,7 +4,6 @@ import BtnGlobal from "./BtnGlobal";
 const Card = ({ id, title, price, mainImg, secondaryImg }) => {
   const [isHovered, setIsHovered] = useState(false);
 
-  
   const getInitialWishlist = () => {
     const storedWishlist = JSON.parse(localStorage.getItem("wishlist"));
     return storedWishlist || [];
@@ -24,28 +23,30 @@ const Card = ({ id, title, price, mainImg, secondaryImg }) => {
         ...prevWishlist,
         { id, title, price, mainImg },
       ]);
-
       // Update localStorage only if the product's id is not already present
       if (!getInitialWishlist().some((product) => product.id === id)) {
         localStorage.setItem(
           "wishlist",
-          JSON.stringify([...getInitialWishlist(), { id, title, price, mainImg }])
+          JSON.stringify([
+            ...getInitialWishlist(),
+            { id, title, price, mainImg },
+          ])
         );
       }
     }
   };
 
   return (
-    <div
-      className="flex flex-col   lg:w-[13rem] xl:w-[17rem]"
-    >
-      <div className="relative" onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}>
+    <div className="flex flex-col   lg:w-[13rem] xl:w-[17rem]">
+      <div
+        className="relative"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
         <img
           src={isHovered ? secondaryImg : mainImg}
           alt={title}
           className="border-blackV border-2"
-          
         />
         {isHovered && (
           <button
@@ -56,30 +57,26 @@ const Card = ({ id, title, price, mainImg, secondaryImg }) => {
           </button>
         )}
       </div>
-      <div className="flex flex-start justify-between my-2 text-sm md:text-base  font-medium">
-        <h2>{title}</h2>
+      <div className="justify-between my-2 text-sm md:text-base  font-medium">
+        <h2 className=" font-bold">{title}</h2>
         <h2>{price}</h2>
       </div>
       <div className="">
         <BtnGlobal
           className="w-full border-2 border-blackV p-1 md:p-2 font-medium"
           content="View Details"
-        /> 
+        />
         <BtnGlobal
           className="hidden lg:block lg:w-full border-2 border-blackV bg-blackV text-white font-medium p-2 my-2 "
           content="Add To Cart "
         />
         <div className="flex justify-center gap-2 md:gap-5">
-        <BtnGlobal
-          className="lg:hidden w-1/2 border-2 border-blackV bg-blackV text-white text-base md:text-xl p-1 md:p-2 my-2 pi pi-shopping-bag rounded-sm"
-        />
-        <BtnGlobal
-          className=" w-1/2 lg:hidden lg:w-full border-2 border-blackV bg-blackV text-white text-base md:text-xl p-1 md:p-2 my-2 pi pi-heart rounded-sm"
-          onClick={addProductToStorage}
-        />
+          <BtnGlobal className="lg:hidden w-1/2 border-2 border-blackV bg-blackV text-white text-base md:text-xl p-1 md:p-2 my-2 pi pi-shopping-bag rounded-sm" />
+          <BtnGlobal
+            className=" w-1/2 lg:hidden lg:w-full border-2 border-blackV bg-blackV text-white text-base md:text-xl p-1 md:p-2 my-2 pi pi-heart rounded-sm"
+            onClick={addProductToStorage}
+          />
         </div>
-        
-
       </div>
     </div>
   );
