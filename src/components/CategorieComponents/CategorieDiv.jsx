bc7ab299be6ce8980f202b7088860c7986063a13
Oussama kersit <kersiit@gmail.com>
2023-12-12 12:47:55 +0100

    add image + add Link

--- src/components/CategorieComponents/CategorieDiv.jsx
+++ src/components/CategorieComponents/CategorieDiv.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import BtnGlobal from "../GlobalComponents/BtnGlobal"
+import { Link } from "react-router-dom";
 
 const CategorieDiv = ({ imgSrc }) => {
   return (
     <div className="w-1/3 border-r border-2 border-blackV relative">
-      <img className="w-full h-full object-cover hover:opacity-90 transition-opacity duration-300 ease-in-out cursor-pointer" src={imgSrc} alt="Image" />
+      <img
+        className="w-full h-full object-cover hover:opacity-90 transition-opacity duration-300 ease-in-out cursor-pointer"
+        src={imgSrc}
+        alt="Image"
+      />
+      <Link to={'/women-products'}>
       <BtnGlobal
-        className="absolute bottom-10 border-2 left-1/2 p-2 w-[30vw] md:p-4 md:w-44  lg:p-4 lg:w-52 transform -translate-x-1/2 bg-white hover:bg-purple-400 border-blackV hover:border-white hover:text-white  font-medium text-base md:text-lg "
+        className="absolute bottom-10 border-2 left-1/2 p-2 w-[30vw] md:p-4 md:w-44  lg:p-4 lg:w-60 transform -translate-x-1/2 bg-white hover:bg-blackA border-blackV hover:border-white hover:text-white font-medium text-base md:text-lg "
         content="View More"
       />
+      </Link>
     </div>
   );
 };
 
 export default CategorieDiv;
+
+// className="absolute bottom-10 border-2 left-1/2 p-2 w-[30vw] md:p-4 md:w-44  lg:p-4 lg:w-52 transform -translate-x-1/2 bg-white hover:bg-purple-400 border-blackV hover:border-white hover:text-white  font-medium text-base md:text-lg "
+
